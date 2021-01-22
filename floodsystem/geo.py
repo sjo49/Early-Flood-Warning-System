b0debc2d1935b3c2c065c244650e3f2aedc13589
bb615 <bb615@cam.ac.uk>
2021-01-22 18:43:57 +0000

    commit new geo to main for task 1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,23 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+import math
+def stations_by_distance(stations, p):
+    tuple_list = []
+    for station in stations:
+        def distance(station):
+            R = 6373.0
+            lat1 = station.coord[0]
+            lon1 = station.coord[1]
+            lat2 = p[0]
+            lon2 = p[1]
+            dlat = lat2 - lat1
+            dlon = lon2 - lon1
+            a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+            distance = R * c
+            return distance
+        tuple_list += (station.name, distance(station))
+        return tuple_list.utils.sorted_by_key[1]
+
+print(stations_by_distance(stations, (50.2, -5.24)))
