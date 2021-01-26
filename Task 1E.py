c1b28d56f0ba2cbf10e2c645db57c109dbaaf54d
Sophie O'Keeffe <sjo49@cam.ac.uk>
2021-01-27 00:51:23 +0400

    Task E corrections

--- Task 1E.py
+++ Task 1E.py
@@ -1,10 +1,7 @@
 from floodsystem.geo import rivers_with_station
 from floodsystem.stationdata import build_station_list
-from floodsystem.geo import stations_by_river
 from floodsystem.geo import rivers_by_station_number
-from floodsystem.utils import sorted_by_key
 
 stations = build_station_list()
 rivers = rivers_with_station(stations)
 print(rivers_by_station_number(stations, 12))
-
