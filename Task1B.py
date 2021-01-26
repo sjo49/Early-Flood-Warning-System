62fbe32f4b9b9b172b468698a81ab55245637861
bb615 <bb615@cam.ac.uk>
2021-01-26 17:08:25 +0000

    1B

--- Task1B.py
+++ Task1B.py
@@ -5,29 +5,29 @@ from floodsystem.utils import sorted_by_key
 # import station list from station data
 from floodsystem.stationdata import build_station_list
 stations = build_station_list()
+
+
 def run():
     list_stations = stations_by_distance(stations, (52.2053, 0.1218))
-    # slice list to get 10 closest stations
+# slice list to get 10 closest stations
     list_close = []
     list_far = []
     list_close.extend(list_stations[:10])
     list_far.extend(list_stations[-10:])
     list_town_close = []
     list_town_far = []
-    # find town corresponding with station
+# find town corresponding with station
     for i in range(len(list_close)):
         for j in range(len(stations)):
             if list_close[i][0] in stations[j].name:
                 list_town_close.append((list_close[i][0], stations[j].town, list_close[i][1]))
-    # return list sorted by distance
-    # repeat above steps for furthest station
+# return list sorted by distance
+# repeat above steps for furthest station
     for x in range(len(list_far)):
         for j in range(len(stations)):
             if list_far[x][0] in stations[j].name:
                 list_town_far.append((list_far[x][0], stations[j].town, list_far[x][1]))
     # return list sorted by distance
-
-    
     return sorted_by_key(list_town_close, 2), sorted_by_key(list_town_far, 2)
 
 
