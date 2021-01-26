306f156ba01e0e1e18debc5d23fda27e17e8baa3
Sophie O'Keeffe <sjo49@cam.ac.uk>
2021-01-26 20:10:19 +0400

    Task D

---
+++ Task 1D.py
@@ -0,0 +1,14 @@
+from floodsystem.geo import rivers_with_station
+from floodsystem import datafetcher
+from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list
+
+stations = build_station_list()
+rivers = rivers_with_station(stations)
+#print(rivers)
+#print(len(rivers))
+
+from floodsystem.geo import stations_by_river
+stations_on_each_river = stations_by_river(stations)
+
+print(stations_on_each_river['River Thames'])
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,23 +6,25 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
-import math
-def stations_by_distance(stations, p):
-    tuple_list = []
-    for station in range(len(stations):
-        def distance(station):
-            R = 6373.0
-            lat1 = station.coord[0]
-            lon1 = station.coord[1]
-            lat2 = p[0]
-            lon2 = p[1]
-            dlat = lat2 - lat1
-            dlon = lon2 - lon1
-            a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
-            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-            distance = R * c
-            return distance
-        tuple_list += (station.name, distance(station))
-        return tuple_list.utils.sorted_by_key[1]
+from . import datafetcher
+from .station import MonitoringStation
+from .stationdata import build_station_list
+stations = build_station_list()
+def rivers_with_station(stations):
+    rivers_list = []
+    for station in stations:
+        rivers_list.append(station.river) #builds list with all rivers
+    a = set(rivers_list) #coverting to set removes duplicates
+    sorted_rivers = sorted(a) #coverts set into alphabetical list of rivers
+    return sorted_rivers
 
+def stations_by_river(stations):
+    rivers = rivers_with_station(stations)
+    river_dict = dict()
+    for i in range(len(rivers)):
+        stations_on_river = []
+        for j in range(len(stations)):
+            if rivers[i] in stations[j].river:
+                stations_on_river.append(stations[j].name)
+            river_dict[rivers[i]] = sorted(stations_on_river)
+    return river_dict
