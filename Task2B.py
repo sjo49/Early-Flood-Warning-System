46bddacd3136f942554129dcdd39d58f0bd006a8
bb615 <bb615@cam.ac.uk>
2021-02-16 15:31:59 +0000

    changes

--- Task2B.py
+++ Task2B.py
@@ -1,6 +1,10 @@
 from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list
-stations = build_station_list
+stations = build_station_list()
+from floodsystem.stationdata import update_water_levels
+update_water_levels(stations)
+
+print(stations[23].relative_water_level())
 
 def run():
     stations_level_over_threshold(stations, 0.8)
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -2,12 +2,11 @@ from .utils import sorted_by_key
 
 def stations_level_over_threshold(stations, tol):
     over_level = []
-    station_list = stations()
-    for station in station_list:
+    for station in stations:
         if station.relative_water_level() is None:
             pass
         elif station.relative_water_level() > tol:
-            over_level.append(station.name, station.relative_water_level)
+            over_level.append((station.name, station.relative_water_level()))
     return sorted_by_key(over_level, 1, True)
 
 
