11bcdad2d80e7e3c2957f9075ea68e057eb9af37
bb615 <bb615@cam.ac.uk>
2021-02-28 11:00:33 +0000

    test changes

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -3,21 +3,21 @@ from .utils import sorted_by_key
 
 def stations_level_over_threshold(stations, tol):
     over_level = []
-    for station in stations:
-        if station.relative_water_level() is None:
+    for i in range(len(stations)):
+        if stations[i].relative_water_level() is None:
             pass
-        elif station.relative_water_level() > tol:
-            over_level.append((station.name, station.relative_water_level()))
+        elif stations[i].relative_water_level() > tol:
+            over_level.append((stations[i].name, stations[i].relative_water_level()))
     return sorted_by_key(over_level, 1, True)
 
 
 def stations_highest_rel_level(stations, N):
     highest = []
-    for station in stations:
-        if station.relative_water_level() is None:
+    for i in range(len(stations)):
+        if stations[i].relative_water_level() is None:
             pass
         else:
-            highest.append((station.name, station.relative_water_level()))
+            highest.append((stations[i].name, stations[i].relative_water_level()))
     highest_sorted = sorted_by_key(highest, 1, True)
     highest_cut = highest_sorted[:N]
     return highest_cut
--- test_flood.py
+++ test_flood.py
@@ -1,14 +1,15 @@
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.flood import stations_level_over_threshold
-from floodsystem.stationdata import build_station_list
-stations = build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
+stations = build_station_list()
+update_water_levels(stations)
 
 
 def test_stations_level_over_threshold():
     levels = stations_level_over_threshold(stations, 0.5)
-    for i in levels:
-        assert [i][1] >= 0.5
-        assert [i][1] >= [i + 1][1]
+    for i in range(len(levels) - 1):
+        assert levels[i][1] >= 0.5
+        assert levels[i][1] >= levels[i + 1][1]
 
 
 test_stations_level_over_threshold()
@@ -17,8 +18,8 @@ test_stations_level_over_threshold()
 def test_stations_highest_rel_level():
     levels = stations_highest_rel_level(stations, 20)
     assert len(levels) == 20
-    for i in levels:
-        assert [i][1] >= [i + 1][1]
+    for i in range(len(levels) - 1):
+        assert levels[i][1] >= levels[i + 1][1]
 
 
 test_stations_highest_rel_level()
