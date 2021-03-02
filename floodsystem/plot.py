195a0c475b7753b30370e36e08877610490fff7c
Sophie O'Keeffe <sjo49@cam.ac.uk>
2021-03-02 18:18:08 +0400

    test_plot modiications

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -7,39 +7,45 @@ def plot_water_levels(station, dates, levels):
     t = dates
     level = levels
 
-    # Plot
-    plt.plot(t, level)
+    if len(t) != len(levels):
+        print("Dates and levels inconsistant")
+    else:
+        # Plot
+        plt.plot(t, level)
 
-    # Add axis labels, rotate date labels and add plot title
-    plt.xlabel('date')
-    plt.ylabel('water level (m)')
-    plt.xticks(rotation=45)
-    plt.title(station)
+        # Add axis labels, rotate date labels and add plot title
+        plt.xlabel('date')
+        plt.ylabel('water level (m)')
+        plt.xticks(rotation=45)
+        plt.title(station)
 
-    # Display plot
-    plt.tight_layout()  # This makes sure plot does not cut off date labels
+        # Display plot
+        plt.tight_layout()  # This makes sure plot does not cut off date labels
 
-    return plt.show()
+        return plt.show()
 
 
 def plot_water_level_with_fit(station, dates, levels, p):
-    # dates between which the levels will be computed to fit to a polynomial
-    date_floats = matplotlib.dates.date2num(dates)
-    x = date_floats
-    y = levels
-    p_coeff = np.polyfit(x-x[0], y, p)
-    poly = np.poly1d(p_coeff)
+    if len(dates) != len(levels):
+        print("Dates and levels inconsistant")
+    else:
+        # dates between which the levels will be computed to fit to a polynomial
+        date_floats = matplotlib.dates.date2num(dates)
+        x = date_floats
+        y = levels
+        p_coeff = np.polyfit(x-x[0], y, p)
+        poly = np.poly1d(p_coeff)
 
-    x1 = np.linspace(x[0], x[-1], 30)
-    typical_low, = plt.plot(x1, np.full(30, station.typical_range[0]), 'g')
-    typical_high, = plt.plot(x1, np.full(30, station.typical_range[1]), 'r')
-    poly_plot, = plt.plot(x1, poly(x1-x[0]), 'bo')
-    plt.legend([typical_low, typical_high, poly_plot],
-               ['typical low level', 'typical high level', 'best-fit polynomial'])
-    plt.xlabel('dates')
-    plt.ylabel('polynomial fit')
-    plt.xticks(rotation=45)
-    plt.title(f'{station.name} - water level best-fit polynomial')
-    plt.tight_layout()
+        x1 = np.linspace(x[0], x[-1], 30)
+        typical_low, = plt.plot(x1, np.full(30, station.typical_range[0]), 'g')
+        typical_high, = plt.plot(x1, np.full(30, station.typical_range[1]), 'r')
+        poly_plot, = plt.plot(x1, poly(x1-x[0]), 'bo')
+        plt.legend([typical_low, typical_high, poly_plot],
+                ['typical low level', 'typical high level', 'best-fit polynomial'])
+        plt.xlabel('dates')
+        plt.ylabel('polynomial fit')
+        plt.xticks(rotation=45)
+        plt.title(f'{station.name} - water level best-fit polynomial')
+        plt.tight_layout()
 
-    return plt.show()
+        return plt.show()
--- test_plot.py
+++ test_plot.py
@@ -10,17 +10,18 @@ class Station:
 
 
 def test_plot_water_levels():
-    # test function can be called - assume matplotlib will produce figure correctly
+    # test function will deal with inappropriate arguments
+    # assume matplotlib will produce figure correctly
     station = 'TestStation'
-    dates = [1, 2, 3, 4, 5]
-    levels = [0.349, 0.526, 0.573, 0.468, 0.384]
-    plot_water_levels(station, dates, levels)
+    dates = [1, 2, 3, 4] # length of 4
+    levels = [0.349, 0.526, 0.573, 0.468, 0.384] # length of 5
+    plot_water_levels(station, dates, levels) # does not return error then test passes
 
 
 def test_plot_water_level_with_fit():
     station = Station('TestStation', [0.3, 0.6])
     dates = [datetime(2020, 12, 30), datetime(2020, 12, 31), datetime(2021, 1, 1),
-             datetime(2021, 1, 2), datetime(2021, 1, 3)]
-    levels = [0.349, 0.526, 0.573, 0.468, 0.384]
+             datetime(2021, 1, 2)] # length of 4
+    levels = [0.349, 0.526, 0.573, 0.468, 0.384] # length of 5
     p = 4
     plot_water_level_with_fit(station, dates, levels, p)
